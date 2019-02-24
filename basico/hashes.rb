b76f69a0cb0235d5430316f0ab7b9a9b8a64c2c3
Victor <victor.fernandes291096@outlook.com>
2019-02-24 13:31:21 -0300

    basico e avancado

---
+++ basico/hashes.rb
@@ -0,0 +1,6 @@
+carro = Hash[nome: 'Civic', marca: 'Honda', cor: 'Preto']
+puts carro[:nome]
+
+carro [:modelo] = 'SI'
+
+puts carro
---
+++ basico/tipos.rb
@@ -0,0 +1,19 @@
+# -- Tipos Ruby --
+# O tipo de uma variável é definido nomomento em que a mesma recebe o valor
+
+produto = 'Macbook Air'
+preco = 5999.99
+quantidade = 10
+disponivel = true
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
+
+quantidade = '9'
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel

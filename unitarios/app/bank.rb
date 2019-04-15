8b84dd5cb4b55365db9263b144e0a0408d9378e6
Aline Pereira Duque <apereira@svlabs.local>
2019-04-15 16:02:57 -0300

    estudos

---
+++ unitarios/app/bank.rb
@@ -0,0 +1,34 @@
+class Conta
+
+    attr_accessor :saldo, :mensagem
+
+    def initialize(saldo)
+        self.saldo = saldo
+    end   
+end
+
+class ContaCorrente
+
+    def saca(valor)
+        if (valor > self.saldo)
+            self.mensagem = 'Saldo insuficiente para saque :('
+        elsif (valor > 700.00)
+            self.mensagem = 'Limite máximo por saque é de R$ 700'
+        else 
+            self.saldo -= valor
+        end    
+    end    
+end
+
+class ContaPoupanca
+
+    def saca(valor)
+        if (valor > self.saldo)
+            self.mensagem = 'Saldo insuficiente para saque :('
+        elsif (valor > 500.00)
+            self.mensagem = 'Limite máximo por saque é de R$ 500'
+        else 
+            self.saldo -= valor
+        end    
+    end    
+end

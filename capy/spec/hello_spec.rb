7221a1960856a6fce6665cb3f2170fb70a02320d
Aline Pereira Duque <apereira@svlabs.local>
2019-04-18 16:18:55 -0300

    estudos

--- capy/spec/hello_spec.rb
+++ capy/spec/hello_spec.rb
@@ -2,7 +2,8 @@ describe 'meu primeiro script' do
 
     it 'visitar a página' do
         
-
+        visit 'https://training-wheels-protocol.herokuapp.com/'
+        expect(page.title).to eql 'Training Wheels Protocol'
     end
 
 end       
